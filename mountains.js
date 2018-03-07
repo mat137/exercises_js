db29803f9941db469ee6ddcbcafd47f6cc5ba3c1
mat137 <mat.pilat@gmail.com>
2018-03-07 19:13:41 +0100

    prprprp

---
+++ mountains.js
@@ -0,0 +1,12 @@
+var MOUNTAINS = [
+  {name: "Kilimandżaro", height: 5895, country: "Tanzania"},
+  {name: "Mount Everest", height: 8848, country: "Nepal"},
+  {name: "Fudżi", height: 3776, country: "Japonia"},
+  {name: "Mont Blanc", height: 4808, country: "Francja/Włochy"},
+  {name: "Vaalserberg", height: 323, country: "Holandia"},
+  {name: "Denali", height: 6168, country: "Stany Zjednoczone"},
+  {name: "Popocatepetl", height: 5465, country: "Meksyk"}
+];
+
+if (typeof module != "undefined" && module.exports)
+  module.exports = MOUNTAINS;
